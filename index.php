32fdb7ca64f95639be0da16ecd923968f2f0af19
cloud <cloud@34.124.223.51>
2023-03-15 09:54:03 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Latihan 1</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Latihan Web Programing</h1>
+
+        <h3>LIST MENU</h3>
+        <p>Makanan</p>
+        <ul>
+            <li>Lawar Plek</li>
+            <li>Babi Guling</li>
+            <li>Bebek Guling</li>
+        </ul>
+
+        <script>
+            document.write("Latihan Web Programing - dg JavaScript");
+        </script>
+
+        <br>
+
+        <?php
+            echo "Latihan Web Programing - dg PHP";
+        ?>
+    </body>
+</html>
